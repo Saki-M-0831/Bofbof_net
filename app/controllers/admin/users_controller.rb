79f12ceb32bb127aa4e037219e24d2f9b36f036a
Saki-M-0831 <sakisaki0831@icloud.com>
2021-07-17 04:43:27 +0900

    spelling correction

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
   def index
-    @users = Users.paginate(page: params[:page], per_page: 8)
+    @users = User.paginate(page: params[:page], per_page: 8)
   end
 end
