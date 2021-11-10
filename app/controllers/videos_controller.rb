aa7b91d15e5f4ca93d5c41e5e5ebed696fdac028
Saki-M-0831 <sakisaki0831@icloud.com>
2021-11-10 17:59:11 +0900

    Search[function] in Video index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,7 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.paginate(page: params[:page], per_page: 4)
+    @q = Video.ransack(params[:q])
+    @videos = @q.result(distinct: true).paginate(page: params[:page], per_page: 4)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/about', to: "pages#about"
-  
 
   namespace :admin do
     get 'pages/home'
