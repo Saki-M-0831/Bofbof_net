904b0c9c4c8702d8219400f5d4ff8e99028ecdd1
Saki-M-0831 <sakisaki0831@icloud.com>
2021-11-18 03:38:28 +0900

    layout improve

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   has_many :likes, as: :object, dependent: :destroy
+
+  validates :content, presence: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
   mount_uploader :picture, PictureUploader
+
+  validates :link, presence: true
+  validates :title, presence: true
+  validates :picture, presence: true
 end
